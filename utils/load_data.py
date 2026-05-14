f2f23ba62865ef8ea83d3cb7bad563b82043495b
Kartikatukam <kartikkatukam08@gmail.com>
2026-05-14 22:23:48 +0530

    Fixed deployment file path

--- utils/load_data.py
+++ utils/load_data.py
@@ -5,7 +5,7 @@ import streamlit as st
 @st.cache_data
 def load_data():
     df = pd.read_csv(
-        "C:\Project\Kitchen_PNL_Dashboard\data\Kittchen PNL Data.xlsx - Sheet 1 - stores.csv"
+        "data/Kittchen PNL Data.xlsx - Sheet 1 - stores.csv"
     )
 
     return df
